bf0be8e71c206ee681568c852280a0f91955b87f
ShireenG13 <shireen.nu2022@gmail.com>
2025-03-06 23:37:27 -0600

    Initial commit

---
+++ src/main/java/com/shireen/refugeeservicesproject/RefugeeServicesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shireen.refugeeservicesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefugeeServicesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefugeeServicesProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shireen/refugeeservicesproject/RefugeeServicesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shireen.refugeeservicesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefugeeServicesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
